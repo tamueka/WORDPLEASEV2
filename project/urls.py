f8dcbe596970a1724b60a9cedbe7d243dbc61b07
Samuel <tamueka@hotmail.com>
2018-11-26 23:21:18 +0100

    Refactorizamos las urls

--- project/urls.py
+++ project/urls.py
@@ -16,11 +16,8 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+
 from django.urls import path, include
-
-from blogs.views import home, blog_detail, blogs, new_post
-from users.views import login, logout, signup
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
