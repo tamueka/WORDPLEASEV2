8ffd52c067b0c3da89a0fa1101eeba57105aaf0b
Samuel <tamueka@hotmail.com>
2018-11-27 01:27:12 +0100

    Utilizamos un serializador para convertir los usuarios obtenidos de la base de datos en una lista de diccionarios

---
+++ users/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.Serializer):
+
+    id = serializers.IntegerField()
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    username = serializers.CharField()
+
