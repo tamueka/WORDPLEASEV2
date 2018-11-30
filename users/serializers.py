aa3227b5dd43d663f9135b1df42d9de0093f0ff4
Samuel <tamueka@hotmail.com>
2018-11-30 21:46:03 +0100

    REfactorizamos los serailizer para que el codigo sea mas mantenible

--- users/serializers.py
+++ users/serializers.py
@@ -2,35 +2,22 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
-class UserListSerializer(serializers.ListSerializer):
+class UserListSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     first_name = serializers.CharField()
     last_name = serializers.CharField()
     username = serializers.CharField()
 
-class UserSerializer(serializers.Serializer):
-
-
-    id = serializers.ReadOnlyField()
-    first_name = serializers.CharField()
-    last_name = serializers.CharField()
-    username = serializers.CharField()
+class UserSerializer(UserListSerializer):
     email = serializers.EmailField()
     password = serializers.CharField()
 
 
     def create(self, validated_data):
         user = User()
-        user.first_name = validated_data.get('first_name')
-        user.last_name = validated_data.get('last_name')
-        user.username = validated_data.get('username')
-        user.email = validated_data.get('email')
-        user.set_password(validated_data.get('password'))
-        user.save()
-        return user
+        return self.update(user, validated_data)
 
     def update(self, instance, validated_data):
-        instance = User()
         instance.first_name = validated_data.get('first_name')
         instance.last_name = validated_data.get('last_name')
         instance.username = validated_data.get('username')
